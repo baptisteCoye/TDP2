a30326da92ecb5f9ce1270a311bb47484b27758d
dread <baptiste.coye@brainify.it>
2016-11-04 01:05:19 +0100

    creation des fichiers de Tests

--- src/testfilecreate.c
+++ src/testfilecreate.c
@@ -5,13 +5,13 @@ void CreateFileTest(int nombreParticule, FILE* file){
     int i;
     fprintf(file,"%d\n", nombreParticule);
     for(i = 0; i < nombreParticule; i++){
-        fprintf(file,"%f %f %f %f %f %f %f", i, i, i, i, i, i, i);
+        fprintf(file,"%f %f %f %f %f\n", i, i, i, i, i);
     }
 }
 
 int main(int argc, char** argv){
     if(argc > 2){
-        FILE* file = fopen(argv[2], "rw");
+        FILE* file = fopen(argv[2], "w+");
         CreateFileTest(atoi(argv[1]), file);
     }
     else fprintf(stdout,"<nombre de particules> <nom du fichier>");
